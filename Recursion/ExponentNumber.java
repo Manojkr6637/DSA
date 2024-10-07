2e3511a0c304dc7bc0c90d01124ff3c7c7098aeb
Manoj Kumar Sah <31730550+Manojkr6637@users.noreply.github.com>
2024-10-07 08:03:11 +0530

    Most optimized version add

--- Recursion/ExponentNumber.java
+++ Recursion/ExponentNumber.java
@@ -6,11 +6,15 @@ public class ExponentNumber {
     static void ExponentNumber(){
 
         //1. Iterative
-          int base=2, pow=3;
+          int base=2, pow=9;
         //  iExponent(base, pow);
         //3. Recursive
         // 2 pow 5=2X2X2X2X2
-        int ans= recursiveExponent(base, pow);
+       // int ans= recursiveExponent(base, pow);
+        //4. Optimized version
+        // 2 pow of 8: (2 pow of 4) pow of 4
+        // 2 pow of 9: 2X(2 pow of 4) pow of 4
+        int ans= optimizeRecursiveExponent(base, pow);
         System.out.println("Recursive Ans:"+ans);
 
     }
@@ -28,5 +32,13 @@ public class ExponentNumber {
         if(pow==0)
             return 1;
         return recursiveExponent(base, pow-1)*base;
+    }
+    static int optimizeRecursiveExponent(int m, int n ){
+        if(n==0)
+            return 1;
+        if(n%2==0)
+            return optimizeRecursiveExponent(m*m,  n/2);
+        else
+             return m*optimizeRecursiveExponent(m*m,  (n-1)/2);
     }
 }
